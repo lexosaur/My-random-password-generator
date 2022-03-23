9d1a2ced19b1946ac3252aa7d5f1f887f2180922
Alexis Garcia <alexis.garciad@gmail.com>
2022-03-22 23:11:34 -0600

    final js changes

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 // Assignment Code
+//Define password variables
 var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var numbers = ["0","1","2","3","4","5","6","7","8","9"]
 var specialCharacters = ["!","@","#","$","%","^","&","*","(",")"]
-//Create generatePassword function
-function generatePassword (){
-}
-
+var selectionArray = [];
 
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
-getPrompts();
+var rightPrompts = getPrompts();
 
-  var password = generatePassword("");
+if (rightPrompts){
+  var newPassword = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  
-  passwordText.value = password;
-
+  passwordText.value = newPassword;
 }
+  else {
+    passwordText.value = "";
+  }
+}
+
+//Create generatePassword function
+function generatePassword (){
+  var password = "";
+  for(var i=0; i<passwordLength; i++) {
+    var randomValue = Math.floor(Math.random()*selectionArray.length);
+    password = password + selectionArray[randomValue];
+  }
+  return password;
+}
+
 
 function getPrompts(){
   //Ask the user how long they want the password to be and convert the input into a number (integer)
   selectionArray = [];
-  passwordLength = parseInt(prompt("How long would you like yous password to be? (8-128 characters)"));
+  passwordLength = parseInt(prompt("How long would you like your password to be? (8-128 characters)"));
 
-  if(isNaN(passwordLength)|| passwordLength < 8|| passwordLength > 128){
+  if (isNaN(passwordLength)|| passwordLength < 8|| passwordLength > 128){
     alert ("Please make sure password length is minimum 8 characters and maximum 128.");
     return false
   }
@@ -40,16 +54,16 @@ function getPrompts(){
     selectionArray = selectionArray.concat(upperCaseLetters)
   }
 
+  if (confirm("Do you want to include numbers in the generated password?")){
+    selectionArray = selectionArray.concat(numbers)
+  }
+
   if (confirm("Do you want to include special characters in the generated password?")){
     selectionArray = selectionArray.concat(specialCharacters)
   }
 
-  if (confirm("Do you want to include numbers in the generated password?")){
-    selectionArray = selectionArray.concat(numbers)
-  }
+
 return true;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
